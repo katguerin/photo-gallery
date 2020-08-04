06f1c81a0a52b49f8d6729b4961469dfec8feab3
katguerin <katguerin55@gmail.com>
2020-08-03 23:30:49 -0700

    tests aren't working but it's compliling and looks good. will ask for help with the tests

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -37,13 +37,15 @@ function Nav(props) {
               About me
             </a>
           </li>
-          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+          <li className={`mx-2 ${contactSelected && "navActive"}`}>
             <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name && !contactSelected && `navActive`
+                currentCategory.name === category.name &&
+                !contactSelected &&
+                `navActive`
               }`}
               key={category.name}
             >
